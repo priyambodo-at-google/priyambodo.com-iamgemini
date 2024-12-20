d3aad3fe0d83b6e50748841730c89b845ec3d6e4
Doddi Priyambodo <doddi@bicarait.com>
2024-12-20 13:26:38 +0000

    doddi

--- iamgemini-gui.v6.0/sh00-set-environment.sh
+++ iamgemini-gui.v6.0/sh00-set-environment.sh
@@ -9,9 +9,11 @@ echo $AR_REPO
 echo $SERVICE_NAME
 
 #authenticate your laptop to google cloud
+gcloud components update
 gcloud init	#Authorizes access and performs other common setup steps.
 gcloud auth login #Authorizes access only.
 gcloud auth application-default login
+gcloud config set project work-mylab-machinelearning
 
 gcloud auth application-default set-quota-project $GCP_PROJECT
 gcloud config set billing/quota_project $GCP_PROJECT
